6bb002805f1312f708f145c3eb56795a4d61be19
Gollim <virtualhowl@gmail.com>
2019-10-07 17:15:48 +0200

    Meshes_1/2
    In this commit we charge the meshes in a array

--- Hydro-Engine/ModuleImporter.cpp
+++ Hydro-Engine/ModuleImporter.cpp
@@ -1,12 +1,8 @@
 #include "Application.h"
 #include "ModuleImporter.h"
+#include "Globals.h"
 
-#include "Assimp/include/cimport.h"
-#include "Assimp/include/scene.h"
-#include "Assimp/include/postprocess.h"
-#include "Assimp/include/cfileio.h"
 
-#pragma comment (lib, "Assimp/libx86/assimp.lib")
 
 
 ModuleImporter::ModuleImporter(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -19,10 +15,69 @@ ModuleImporter::~ModuleImporter()
 
 bool ModuleImporter::Start()
 {
+	//debugger
 	aiLogStream stream = aiGetPredefinedLogStream(aiDefaultLogStream_DEBUGGER, nullptr);
 	aiAttachLogStream(&stream);
 
+	LoadFBX("Assets/warrior.FBX");
+
 	return true;
+}
+
+bool ModuleImporter::LoadFBX(const std::string & Filename)
+{
+	bool Ret = true;
+	
+	Assimp::Importer Importer;
+
+	const aiScene* pScene = Importer.ReadFile(Filename.c_str(), aiProcess_Triangulate | aiProcessPreset_TargetRealtime_MaxQuality); // el aiProcess_Triangulate sirve para transformar las caras cuadras en triangulos
+
+	if (pScene) {
+		Ret = SceneToMesh(pScene);
+	}
+	else {
+		printf("Error parsing '%s': '%s'\n", Filename.c_str(), Importer.GetErrorString());
+	}
+
+	return Ret;
+}
+
+bool ModuleImporter::SceneToMesh(const aiScene * FBXScene)
+{
+	//mNumMeshes sirve para entrar en el tamaño, y mMeshes sirve para entrar en la array de meshes de la escena.
+	// Para ir una por una y separar las diversas meshes.
+	for (uint i = 0; i < FBXScene->mNumMeshes; i++) {
+		const aiMesh* sMesh = FBXScene->mMeshes[i];
+		InitMesh(i, sMesh);
+	}
+	return true;
+}
+
+void ModuleImporter::InitMesh(uint Index, const aiMesh * sMesh)
+{
+	Mesh SceneMesh;
+
+	SceneMesh.num_vertex = sMesh->mNumVertices;
+	SceneMesh.vertex = new float[SceneMesh.num_vertex * 3]; // Lo multiplicamos por 3 porque hay 3 valores por vertice
+	memcpy(SceneMesh.vertex, sMesh->mVertices, sizeof(float) * SceneMesh.num_vertex * 3); // Lo multiplicamos por el sizeof(float) yaque el memcpy trabaja en malditos bits.
+	
+
+	 // copy faces
+	if (sMesh->HasFaces())
+	{
+		SceneMesh.num_index = sMesh->mNumFaces * 3;
+		SceneMesh.index = new uint[SceneMesh.num_index]; // assume each face is a triangle // Que esto a la vez ya lo hacemos antes al cargar el fbx.
+		for (uint i = 0; i < sMesh->mNumFaces; ++i)
+		{
+			if (sMesh->mFaces[i].mNumIndices != 3)
+			{
+				LOG("WARNING, geometry face with != 3 indices!");
+			}
+			else
+				memcpy(&SceneMesh.index[i * 3], sMesh->mFaces[i].mIndices, 3 * sizeof(uint));
+		}
+	}
+	scene_meshesh_xd.push_back(&SceneMesh);
 }
 
 update_status ModuleImporter::PreUpdate(float dt)
@@ -42,7 +97,7 @@ update_status ModuleImporter::PostUpdate(float dt)
 
 bool ModuleImporter::CleanUp()
 {
-
+	//Debugger
 	aiDetachAllLogStreams();
 	return true;
 }
--- Hydro-Engine/ModuleImporter.h
+++ Hydro-Engine/ModuleImporter.h
@@ -1,6 +1,28 @@
 #pragma once
 
 #include "Module.h"
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/Importer.hpp"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
+#include "Assimp/include/cfileio.h"
+
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
+
+struct Mesh {
+
+	uint id_index = 0; // index in VRAM
+	uint num_index = 0;
+	uint* index = nullptr;
+
+	uint id_vertex = 0; // unique vertex in VRAM
+	uint num_vertex = 0;
+	float* vertex = nullptr;
+
+	
+
+};
 
 class ModuleImporter : public Module
 {
@@ -9,9 +31,20 @@ public:
 	~ModuleImporter();
 
 	bool Start();
+	// Con esta funcion cargamos el FBX.
+	bool LoadFBX(const std::string& Filename);
+	// Una vez esta cargado el fbx (FBXScene) la descomprimimos en las diferentes meshes y las cargamos una a una.
+	bool SceneToMesh(const aiScene* FBXScene);
+	// Esta es la funcion de carga de meshes, tenemos ademas el scene_meshes_xd que es donde guardaremos cada una de las meshes para luego pintarlas.
+	void InitMesh(uint Index, const aiMesh* sMesh);
+
 	update_status PreUpdate(float dt);
 	update_status Update(float dt);
 	update_status PostUpdate(float dt);
 
 	bool CleanUp();
+private:
+	std::vector<Mesh*> scene_meshesh_xd;
 };
+
+
