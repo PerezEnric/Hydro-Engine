50a364ae56c49ff6a721f58cdf24a6cac3ae4f48
PerezEnric <enriic19@gmail.com>
2019-11-15 18:58:50 +0100

    Starting frustum

--- Hydro-Engine/Component_Camera.cpp
+++ Hydro-Engine/Component_Camera.cpp
@@ -3,6 +3,18 @@
 
 Component_Camera::Component_Camera(GameObject* gameObject, COMPONENT_TYPE type)
 {
+	frustum.type = FrustumType::PerspectiveFrustum;
+
+	frustum.pos = float3::zero;
+	frustum.front = float3::unitZ;
+	frustum.up = float3::unitY;
+
+	//Common distance between near and far planes
+	frustum.nearPlaneDistance = 0.1f; 
+	frustum.farPlaneDistance = 100.0f;
+
+	frustum.verticalFov = 90 * RADTODEG; //We have to depen on a certain FOV. Normally is the vertical FoV. We assign 90 degrees because is normally used in PC games (Wikipedia rules)
+	frustum.horizontalFov = 2 * atanf(tan(frustum.verticalFov * 0.5) * 1.78f); //1.78 is the aspect ratio for 16:9 => 1920x1080p
 }
 
 Component_Camera::~Component_Camera()
